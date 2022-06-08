45e9f2926d72cb0f44a2b926836da775ca054332
TIMMASTER <73167144+TimM4ster@users.noreply.github.com>
2022-06-08 12:42:08 +0200

    Added template for studis

--- src/main/java/h02/Main.java
+++ src/main/java/h02/Main.java
@@ -1,16 +1,133 @@
 package h02;
 
+import fopbot.Robot;
+import fopbot.World;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Main entry point in executing the program.
  */
 public class Main {
+    // Delay between each action in Fopbot-World (world), for example:
+    // Waits 1000ms between each .move() call
+    public static final int DELAY = 1000;
+
+    // Generates random int between 4 (inclusive) and 10 (exclusive)
+    public static int getRandomWorldSize() {
+        return 4 + ThreadLocalRandom.current().nextInt(6);
+    }
+
+    // ---------------- DO NOT CHANGE ANYTHING ABOVE THIS LINE ---------------
+
+    public static void main(String[] args) {
+        int cols = getRandomWorldSize();
+        int rows = getRandomWorldSize();
+        World.setSize(cols, rows);
+        World.setDelay(DELAY);
+        World.setVisible(true);
+        System.out.println("Size of world: " + cols + "x" + rows);
+
+        // Put your code here
+
+    }
 
     /**
-     * Main entry point in executing the program.
+     * Initialize allRobots array for given pattern and world size.
      *
-     * @param args program arguments, currently ignored
+     * @param pattern           The pattern for the robots.
+     * @param numberOfColumns   Number of columns in world.
+     * @param numberOfRows      Number of rows in world.
+     * @return                  Correctly initialized allRobots array.
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static Robot[] initializeRobotsPattern(boolean[][] pattern, int numberOfColumns, int numberOfRows) {
+        int numberOfRobots = 0;
+
+        // Put your code here
+
+        Robot[] allRobots = new Robot[numberOfRobots];
+
+        // Put your code here
+
+        return allRobots;
+    }
+
+    /**
+     * Lets all robots in the given array walk to the right while also putting down coins.
+     * If robots leave the world they are set to null.
+     * After the steps are made, if more than three robots exist, three of them change their index.
+     * If more than 2 components of the array are null, the array is reduced by the amount of null components.
+     *
+     * @param allRobots   Array containing all the robots.
+     */
+    public static void letAllRobotsGo(Robot[] allRobots) {
+
+        // Put your code here
+
+    }
+
+    /**
+     * Returns how many of the components of the given robot-array are null.
+     *
+     * @param allRobots   The Robot-array.
+     * @return            True, if array contains robot.
+     */
+    public static int numberOfNullElements(Robot[] allRobots) {
+
+        // Put your code here
+
+        return 0;
+    }
+
+    /**
+     * Checks whether given robot can move to the right without leaving the world.
+     *
+     * @param roby    The robot.
+     * @return        True, if the robot can move to the right.
+     */
+    public static boolean checkMoveToRight(Robot roby) {
+
+        // Put your code here
+
+        return false;
+    }
+
+    /**
+     * Creates an array containing three (pseudo-) random int values from 0 (inclusive) to given parameter (exclusive).
+     *
+     * @param bound   The upper bound for the int values.
+     * @return        The array.
+     */
+    public static int[] generateThreeRandomIndices(int bound) {
+
+        // Put your code here
+
+        return null;
+    }
+
+    /**
+     * Sorts the given 3 valued array from lowest to highest.
+     *
+     * @param array   The array to be sorted.
+     */
+    public static void sortArray(int[] array) {
+
+        // Put your code here
+
+    }
+
+    /**
+     * Swaps three robots in given robot array.
+     * Robot at index i will later be at index j.
+     * Robot at index j will later be at index k.
+     * Robot at index k will later be at index i.
+     *
+     * @param indices       Array containing indices i, j and k.
+     * @param allRobots     Array containing the robots.
+     */
+    public static void swapRobots(int[] indices, Robot[] allRobots) {
+
+        // Put your code here
+
     }
 }
